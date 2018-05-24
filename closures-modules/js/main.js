d9a4644a559d3af9fa4efe3255060e7c674f579d
robalb <alberto.ventafridda@gmail.com>
2018-05-24 14:25:03 +0200

    added onj folder

--- closures-modules/js/main.js
+++ closures-modules/js/main.js
@@ -1,1 +1,61 @@
-    
+var module = function(){
+  // configuration, change things here
+  var config = {
+    CSS:{
+     classes:{
+       hover:'hover',
+       active:'current',
+       jsEnabled:'js'
+     },
+     ids:{
+       container:'maincontainer'
+     } 
+    },
+    timeout:2000,
+    userID:'chrisheilmann'
+  };
+ 
+  // start of main code
+  // TODO: this will call the private metohod that creates all the callbacks and ui listeners.
+  function init(){
+    // check if the first argument is an object
+    var a = arguments;
+    if(isObj(a[ 0 ])){
+      var cfg = a[ 0 ];
+ 
+      // loop through arguments and alter the configuration
+      for(var i in cfg){
+        setConfig(config,i,cfg[i]);
+      }
+    }
+  };
+ 
+  function setConfig(o,p,v){
+    // loop through all the properties of he object
+    for(var i in o){
+      // when the value is an object call this function recursively
+      if(isObj(o[i])){
+        setConfig(o[i],p,v);
+ 
+      // otherwise compare properties and set their value accordingly
+      } else {
+        if(i === p){o[p] = v;};
+      }
+    }
+  };
+ 
+  // tests if a parameter is an object (and not an array)
+  function isObj(o){
+    return (typeof o === 'object' && typeof o.splice !== 'function');
+  }
+  // ... more methods and other code ...
+  // make init a public method
+  return {
+    init:init
+  };
+}();
+module.init({
+    //TODO: this will initialize the main module with php received data as parameters
+  container:'header',
+  'timeout':1000
+});
---
+++ obj-inheritance/js/main.js
@@ -0,0 +1,67 @@
+
+/*
+Function Car()
+
+__proto__: function {
+    //all the Function stuff
+}
+prototype:{
+    dumpData: function dumpData(){...}
+    constructor: function Car(){}
+    __proto__: Object{
+        //all the object stuff
+    }
+}
+
+
+
+*/
+
+
+function Car(a,b){
+    this.a = a || "z";
+    this.b = b || "z";
+}
+Car.prototype.dumpData = function(){
+    return this.a+" "+this.b;
+}
+
+
+
+
+
+/*
+a: "aa1",
+b: "bb1",
+__proto__: {
+​​
+    constructor: function Car(),
+    ​​
+    dumpData: function dumpData(),
+    ​​
+    __proto__: Object {
+        //all the Object stuff
+    }
+
+}
+*/
+var car1 = new Car("aa1","bb1");
+
+/*
+a: "aa2",
+b: "bb2",
+__proto__: {
+​​
+    constructor: function Car(),
+    ​​
+    dumpData: function dumpData(),
+    ​​
+    __proto__: Object {
+        //all the Object stuff
+    }
+
+}
+*/
+var car2 = new Car("aa2","bb2");
+
+
