b3d2d7d8ffeabafb45b86c0efefacb2119c26fea
robalb <alberto.ventafridda@gmail.com>
2018-07-11 16:32:11 +0200

    gpg test

--- kata/kata2.js
+++ kata/kata2.js
@@ -1,6 +1,8 @@
 https://www.codewars.com/kata/multi-line-task-hello-world-without-letters/train/javascript
 You are provided with:
 
+
+
 let $ = 'CSg';
 
 You need to change $ into a function that returns the string 'Hello, world!'.
