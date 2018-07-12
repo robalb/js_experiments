a57652269543318a63beeadd668dc78d93db8625
robalb <alberto.ventafridda@gmail.com>
2018-07-12 11:36:09 +0200

    q

--- reddit api/auth.js
+++ reddit api/auth.js
@@ -4,14 +4,16 @@
    
    // Get the `code` querystring param (assuming the user was redirected from reddit)
    var code = new URL(window.location.href).searchParams.get('code');
+   var r;
    snoowrap.fromAuthCode({
      code: code,
      userAgent: '',
      clientId: 'WJ25uK_7RfTKCg',
      redirectUri: 'https://robalb.github.io/js_experiments/reddit%20api/auth.html'
-   }).then(r => {
+   }).then(_r => {
      // Now we have a requester that can access reddit through the user's account
        console.log(r)
+       r = _r
    })   
 
    
